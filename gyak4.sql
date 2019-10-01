e170b615167235aae4fe1b0781d66f48a86501cc
csonyi <csonyi@users.noreply.github.com>
2019-10-01 11:41:23 +0200

    Update gyak4.sql

--- gyak4.sql
+++ gyak4.sql
@@ -19,3 +19,43 @@ select *
     from (select nev from szeret), (select gyumolcs from szeret where nev = 'Micimackó')
 MINUS
 select * from szeret;
+
+
+--------------------------------------------------------------------------------
+-- Kik azok a dolgozók, akik 1982.01.01 után léptek be a céghez?
+--------------------------------------------------------------------------------
+
+select dnev, belepes
+    from dolgozo
+        where belepes > TO_DATE('1982-01-01', 'YYYY-MM-DD');
+        
+--------------------------------------------------------------------------------
+--Adjuk meg azon dolgozókat, akik nevének második betűje 'A'.
+--------------------------------------------------------------------------------
+
+select dnev
+    from dolgozo
+        where dnev like '_A%';
+        
+--------------------------------------------------------------------------------
+-- Adjuk meg azon dolgozókat, akik nevében van legalább két 'L' betű. 
+--------------------------------------------------------------------------------
+
+select dnev
+    from dolgozo
+        where dnev like '%L%L%';
+        
+--------------------------------------------------------------------------------
+-- Adjuk meg a dolgozók nevének utolsó három betűjét.
+--------------------------------------------------------------------------------
+
+select SUBSTR(dnev, -3, 3)
+    from dolgozo;
+    
+--------------------------------------------------------------------------------
+-- Adjuk meg a dolgozók fizetéseinek négyzetgyökét két tizedesre, és ennek egészrészét. 
+--------------------------------------------------------------------------------
+
+select ROUND(SQRT(fizetes), 2), FLOOR(ROUND(SQRT(fizetes), 2))
+    from dolgozo;
+  
